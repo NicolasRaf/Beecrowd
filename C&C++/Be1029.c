1cf5a65e3d73ceedfc6349805be8f12bfc5414a2
NicolasRaf <nicolasalves136@gmail.com>
2024-10-18 15:15:31 -0300

    Add: Be1029

--- C&C++/Be1029.c
+++ C&C++/Be1029.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
 
-char exam(avarage){
-    float examNote;
-}
-  
-char verifyStatus(avarage){
+void exam(float avarage){
+    float examNote = 0.0;
+    scanf("%f", &examNote);
+
+    printf("Media: %.1f\n", avarage);
+    printf("Aluno em exame.\n");    
+    avarage = (avarage + examNote)/2;
+
+    printf("Nota do exame: %.1f\n", examNote);
+
     if(avarage < 5){
-        return "Aluno reprovado.";
-    }
-    else if (avarage >= 7){
-        return "Aluno aprovado.";
+        printf("Aluno reprovado.\n");
+    }else{
+        printf("Aluno aprovado.\n");
     }
 
-    return exam(avarage);
+    printf("Media final: %.1f\n", avarage);
+}
+  
+void verifyStatus(float avarage){
+    if(avarage < 5){
+        printf("Media: %.1f\n", avarage);
+        printf("Aluno reprovado.\n");
+
+        return;
+    }
+    else if (avarage >= 7){
+        printf("Media: %.1f\n", avarage);
+        printf("Aluno aprovado.\n");
+        return;
+    }
+
+    exam(avarage);
 }
 
 int main(){
-    float notes[4], avarage;
+    float notes[4], avarage = 0.0;
     scanf("%f %f %f %f", &notes[0], &notes[1], &notes[2], &notes[3]);
 
-    avarage += (notes[0] + notes[1] + notes[2] + notes[3]) /4;
-    printf(verifyStatus(avarage));           
+    avarage += ((notes[0] * 2.0) + (notes[1] * 3.0) + (notes[2] * 4.0) + notes[3]) / 10.0;
+    verifyStatus(avarage);         
     
     return 0;
 }
